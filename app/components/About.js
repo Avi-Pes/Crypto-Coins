c7545fa6a736cd9f7d377df57eb9b44eaf224fe9
Avi Pesach <avi.coolzool@gmail.com>
2023-05-21 04:17:33 +0300

    added About Page first layout

--- app/components/About.js
+++ app/components/About.js
@@ -1,8 +1,8 @@
 "use strict"
 
 function renderAbout() {
-    DOM.controllersBox.innerHTML = ""
-    DOM.contentBox.innerHTML = "<h2></h2>"
+    DOM.controllersBox.innerHTML = "<h2>About Page</h2>"
+    DOM.contentBox.innerHTML = ""
     // TODO
     // personal details
     // project description
@@ -33,17 +33,15 @@ function getAboutContainer() {
     title.style.fontSize = '2em';
     title.style.marginBottom = '1em';
     title.textContent = 'About Me';
-    container.appendChild(title);
 
     // Create image
     let img = document.createElement('img');
     img.src = 'https://media.licdn.com/dms/image/C4E03AQEV4ZneNcD4Ag/profile-displayphoto-shrink_800_800/0/1612371337590?e=1690416000&v=beta&t=pMP8OHpRNq0zKLy90HbfwVfyZM5LupCemvocC6QXK8U';
     img.alt = "That's me";
-    img.style.maxWidth = '100%';
+    img.style.width = '30vw';
     img.style.height = 'auto';
     img.style.borderRadius = '50%';
     img.style.marginBottom = '1em';
-    container.appendChild(img);
 
     // Create info paragraph
     let info = document.createElement('p');
@@ -51,21 +49,18 @@ function getAboutContainer() {
     info.style.textAlign = 'center';
     info.style.marginBottom = '2em';
     info.textContent = "Hi, I'm Avi Pesach, \n" + "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et ex porta, porttitor leo in, lacinia sem. Etiam laoreet in.";
-    container.appendChild(info);
 
     // Create project title
     let projectTitle = document.createElement('h2');
     projectTitle.style.fontSize = '1.5em';
     projectTitle.style.marginBottom = '1em';
     projectTitle.textContent = 'My Project';
-    container.appendChild(projectTitle);
 
     // Create project paragraph
     let projectInfo = document.createElement('p');
     projectInfo.style.maxWidth = '800px';
     projectInfo.style.textAlign = 'center';
     projectInfo.textContent = "Integer eu elementum ipsum. Donec varius tellus ac purus luctus condimentum. Phasellus id condimentum nisi, ut mollis tellus. Nullam non eleifend ex. Sed consequat in lorem sit amet congue. Sed.";
-    container.appendChild(projectInfo);
-
+    container.append(title, img, info, projectTitle, projectInfo)
     return container
 }
