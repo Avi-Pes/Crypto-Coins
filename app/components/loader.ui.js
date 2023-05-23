ba815d2eec5718387c41e69974455a14bae5e8e2
Avi Pesach <avi.coolzool@gmail.com>
2023-05-23 23:24:23 +0300

    finished ABOUT page including texts and styles

--- app/components/loader.ui.js
+++ app/components/loader.ui.js
@@ -5,15 +5,27 @@ function toggleLoader(destination, remove = false) {
     if (remove) {
         destination?.querySelector('.loader')?.remove()
     } else {
-        const loader = getLoader()
+        const loader = getBsLoader()
         destination?.append(loader)
     }
 
 }
 
-function getLoader() {
-    const loader = document.createElement('h3')
-    loader.classList.add("loader", "text-success")
-    loader.innerText = "Loading..."
-    return loader
+function getBsLoader() {
+
+    const wrapper = document.createElement('div')
+    wrapper.classList.add('d-flex', 'justify-content-center')
+    const loader = document.createElement('div')
+    loader.classList.add('loader', 'spinner-border', 'text-info')
+    loader.role = 'status'
+    const span = document.createElement('span')
+    span.classList.add('visually-hidden')
+    span.innerText = "Loading..."
+
+    loader.append(span)
+    wrapper.append(loader)
+
+    return wrapper
+
+
 }
